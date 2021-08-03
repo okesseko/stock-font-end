5a5bd70cd57b3ce59c340612db14b1e249e67e1f
unknown <okesseko@gmail.com>
2021-08-03 21:45:32 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Main from "./pages/main";
 import EchartExample from "./pages/echart-example";
-import RouterLink from "./component/router-link"
+import RouterLink from "./component/router-link";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <RouterLink />
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/echart-example" component={EchartExample} />
-        <Route path="/404" component={() => <div>page not find</div>} />
-        <Redirect from="*" to="/404" />
-      </Switch>
+      <div className="flex my-10">
+        <RouterLink />
+        <div className="flex-1">
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/echart-example" component={EchartExample} />
+            <Route path="/404" component={() => <div>page not find</div>} />
+            <Redirect from="*" to="/404" />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   );
 };
