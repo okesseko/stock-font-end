c6c298b34639350d2efedd1fff34d72e083e5c6c
unknown <okesseko@gmail.com>
2021-08-06 21:20:09 +0800

    設定axios

---
+++ src/environment/api.js
@@ -0,0 +1,14 @@
+import axios, { Method } from "axios";
+
+export const defaultAxios = axios.create({
+  baseURL: "http://220.141.212.175:8080/",
+});
+
+// api
+
+export const api = {
+  getOrder: { url: "api/order", method: "GET" },
+  postOrder: { url: "api/order", method: "POST" },
+  deleteOrder: { url: "api/order", method: "DELETE" },
+  getDisplay: { url: "api/display/tickRange", method: "GET" },
+};
