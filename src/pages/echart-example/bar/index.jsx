55ad78b0da0fe4bdb46e2d015e251c4caf4f2d72
unknown <okesseko@gmail.com>
2021-08-06 20:29:55 +0800

    模擬自動產生資料行為

--- src/pages/echart-example/bar/index.jsx
+++ src/pages/echart-example/bar/index.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import ReactECharts from "echarts-for-react";
 import { data } from "../mock-data";
 
-const options = {
-  tooltip: {
-    trigger: "axis",
-    axisPointer: {
-      type: "shadow",
-    },
-  },
-  grid: {
-    left: "3%",
-    right: "4%",
-    bottom: "3%",
-    containLabel: true,
-  },
-  xAxis: [
-    {
-      type: "category",
-      data: [100, 105, 106, 107, 108, 120, 130],
-    },
-  ],
-  yAxis: [
-    {
-      type: "value",
-    },
-  ],
-  series: [
-    {
-      name: "成交量",
-      type: "bar",
-      data: [10, 52, -520, 334, 390, 330, 220],
-      itemStyle: {
-        color: function (p) {
-          return p.value < 0 ? "red" : "green";
-        },
+const BarChart = ({ data = {} }) => {
+  const options = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
       },
     },
-  ],
-};
-const BarChart = () => {
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: Object.keys(data),
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+      },
+    ],
+    series: [
+      {
+        name: "成交量",
+        type: "bar",
+        data: Object.values(data),
+        itemStyle: {
+          color: function (p) {
+            return p.value < 0 ? "red" : "green";
+          },
+        },
+      },
+    ],
+  };
   return <ReactECharts option={options} />;
 };
 
--- src/pages/echart-example/index.jsx
+++ src/pages/echart-example/index.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Candlestick from "./candlestick";
-import BarChart from "./bar"
-import BarLineChart from "./bar-line"
+import BarChart from "./bar";
+import BarLineChart from "./bar-line";
 
 const EchartExample = () => {
+  const [barData, setBarData] = useState({});
+  const barRef = useRef();
+  barRef.current=barData
+  useEffect(() => {
+    function renderData() {
+      const random = Math.floor(Math.random() * 20 + 50);
+      if (!barRef.current[random]) {
+        setBarData((barData) => ({ ...barData, [random]: Math.floor(Math.random() * 20 + 1)}));
+      } else {
+        setBarData((barData) => ({
+          ...barData,
+          [random]: barData[random] + 1,
+        }));
+      }
+    }
+    const barLoop = setInterval(() => {
+      renderData();
+    }, 200);
+    return () => {
+      clearInterval(barLoop);
+    };
+  }, []);
   return (
-    <div >
-      <Candlestick />
-      <BarChart/>
-      <BarLineChart/>
+    <div>
+      {/* <Candlestick /> */}
+      <BarChart data={barData} />
+      {/* <BarLineChart /> */}
     </div>
   );
 };
