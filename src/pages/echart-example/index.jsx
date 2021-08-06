37d9838a7a92437873194a8778c4e4192d484c2d
Rueifong <karta833@gmail.com>
2021-08-06 17:56:42 +0800

    新增參數設定介面 設定電腦自動下委託單

--- src/pages/echart-example/index.jsx
+++ src/pages/echart-example/index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Candlestick from "./candlestick";
 import BarChart from "./bar"
 import BarLineChart from "./bar-line"
+import Settings from "./settings"
 
 const EchartExample = () => {
   return (
-    <div >
+    <div>
       <Candlestick />
       <BarChart/>
       <BarLineChart/>
+      <Settings />
     </div>
   );
 };
