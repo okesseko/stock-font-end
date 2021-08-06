b963173277e0ae841f443dfa689c175b48a9fe8b
Rueifong <karta833@gmail.com>
2021-08-06 23:36:03 +0800

    bar圖表串接api

--- src/pages/auto-chart/index.jsx
+++ src/pages/auto-chart/index.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useRef, useState } from "react";
 import Settings from "./settings";
 import BarChart from "../echart-example/bar"
+import { defaultAxios, api } from "../../environment/api";
+import { obj } from "duplexify";
+
 const AutoChart = () => {
   const [barData, setBarData] = useState({});
   const barRef = useRef();
   barRef.current = barData;
   useEffect(() => {
     function renderData() {
-      const random = Math.floor(Math.random() * 20 + 50);
-      if (!barRef.current[random]) {
-        setBarData((barData) => ({
-          ...barData,
-          [random]: Math.floor(Math.random() * 20 + 1),
-        }));
-      } else {
-        setBarData((barData) => ({
-          ...barData,
-          [random]: barData[random] + 1,
-        }));
-      }
+      defaultAxios({
+        url: api.getDisplay.url,
+        method: api.getDisplay.method,
+      }).then((res) => {
+        const data = res.data;
+        console.log('chart data', data);
+
+        let chartData = {};
+        data.tickRange.reverse().map(function(obj) {
+          if (obj.price < data.matchPrice) {
+            chartData[obj.price] = -obj.buyQuantity;
+          } else if (obj.price > data.matchPrice) {
+            chartData[obj.price] = obj.sellQuantity;
+          }
+          console.log(obj.price);
+        })
+
+        const ordered = Object.keys(chartData).sort().reduce(
+          (obj, key) => { 
+            obj[key] = chartData[key]; 
+            return obj;
+          }, 
+          {}
+        );
+
+        console.log('chart data 2 ', chartData, ordered);
+
+        setBarData(() => (
+          ordered
+        ));
+      });
     }
     const barLoop = setInterval(() => {
       renderData();
